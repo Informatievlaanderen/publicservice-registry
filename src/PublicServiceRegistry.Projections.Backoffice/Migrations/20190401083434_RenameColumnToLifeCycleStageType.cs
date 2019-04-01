f89a5d3ee014680c625fa27f480f1a3312569ca4
koenmetsu <koen.metsu@kb.vlaanderen.be>
2019-04-01 10:40:33 +0200

    feat: dvr-116: rename db column to LifeCycleStageType

---
+++ src/PublicServiceRegistry.Projections.Backoffice/Migrations/20190401083434_RenameColumnToLifeCycleStageType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PublicServiceRegistry.Projections.Backoffice.Migrations
+{
+    public partial class RenameColumnToLifeCycleStageType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LifeCycleStage",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "PublicServiceLifeCycleList",
+                newName: "LifeCycleStageType");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "LifeCycleStageType",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "PublicServiceLifeCycleList",
+                newName: "LifeCycleStage");
+        }
+    }
+}
