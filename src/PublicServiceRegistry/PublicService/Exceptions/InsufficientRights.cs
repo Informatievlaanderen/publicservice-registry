1fa041b13f045e56c1fff738b1665df0bcd8b595
koenmetsu <koen.metsu@kb.vlaanderen.be>
2019-05-31 12:54:17 +0200

    style: dvr-92: improve aesthetics

--- src/PublicServiceRegistry/PublicService/Exceptions/InsufficientRights.cs
+++ src/PublicServiceRegistry/PublicService/Exceptions/InsufficientRights.cs
@@ -10,7 +10,7 @@ namespace PublicServiceRegistry.PublicService.Exceptions
 
     public class IpdcCodeWasAlreadySet: PublicServiceRegistryException
     {
-        private const string ExceptionMessage = "Op deze dienstverlening is reeds een ipdc code toegewezen." +
+        private const string ExceptionMessage = "Op deze dienstverlening is reeds een ipdc code toegewezen. " +
                                                 "Gelieve eerst de vorige ipdc code te verwijderen vooraleer een nieuwe toe te kennen.";
 
         public IpdcCodeWasAlreadySet()
