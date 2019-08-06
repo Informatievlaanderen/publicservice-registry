9323b55d108c83bcaf776c2111bdb83a65c38b25
David Cumps <david@cumps.be>
2019-08-06 14:52:11 +0200

    fix: correct nginx command

--- src/PublicServiceRegistry.UI/init.sh
+++ src/PublicServiceRegistry.UI/init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-CONTAINERID=$(curl -s http://169.254.170.2/v2/metadata | jq -r ".Containers[] | select(.Labels[\"com.amazonaws.ecs.container-name\"] | contains(\"basisregisters-\") and contains(\"-ui\")) | .DockerId")
-
 echo "window.dienstverleningVersion=\"v2-$API_VERSION\";" >> /usr/share/nginx/html/config.js
 echo "window.dienstverleningApiEndpoint=\"$API_ENDPOINT\";" >> /usr/share/nginx/html/config.js
 
-nginx -g daemon off
+nginx -g 'daemon off;'
