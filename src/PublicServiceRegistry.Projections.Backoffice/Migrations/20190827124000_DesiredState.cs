1acfaa7970495fbb0895f3bd27254c1b2b491a77
David Cumps <david@cumps.be>
2019-08-27 14:40:35 +0200

    fix: use new desiredstate columns for projections

---
+++ src/PublicServiceRegistry.Projections.Backoffice/Migrations/20190827124000_DesiredState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PublicServiceRegistry.Projections.Backoffice.Migrations
+{
+    public partial class DesiredState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DesiredState",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "ProjectionStates",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DesiredStateChangedAt",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "ProjectionStates",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DesiredState",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "ProjectionStates");
+
+            migrationBuilder.DropColumn(
+                name: "DesiredStateChangedAt",
+                schema: "PublicServiceRegistryBackoffice",
+                table: "ProjectionStates");
+        }
+    }
+}
